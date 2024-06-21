52e36a7920ad0ecb072ba76ccb2a3efa5772199e
Charlie <ben02060846@gmail.com>
2024-06-21 16:40:39 +0800

    Add token factory create and update logic

--- x/tokenfactory/types/expected_keepers.go
+++ x/tokenfactory/types/expected_keepers.go
@@ -10,12 +10,20 @@ import (
 type AccountKeeper interface {
 	GetAccount(context.Context, sdk.AccAddress) sdk.AccountI // only used for simulation
 	// Methods imported from account should be defined here
+
+	GetModuleAddress(name string) sdk.AccAddress
+
+	GetModuleAccount(ctx context.Context, moduleName string) sdk.AccountI
 }
 
 // BankKeeper defines the expected interface for the Bank module.
 type BankKeeper interface {
-	SpendableCoins(context.Context, sdk.AccAddress) sdk.Coins
+	SendCoins(ctx context.Context, fromAddr sdk.AccAddress, toAddr sdk.AccAddress, amt sdk.Coins) error
 	// Methods imported from bank should be defined here
+
+	MintCoins(ctx context.Context, moduleName string, amt sdk.Coins) error
+
+	SpendableCoins(ctx context.Context, addr sdk.AccAddress) sdk.Coins
 }
 
 // ParamSubspace defines the expected Subspace interface for parameters.
